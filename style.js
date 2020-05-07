ede23bedadf9f08551de04aab4b3bb4e0018dbfa
keji <tomitoyo.93@gmail.com>
2020-05-07 03:00:45 +0100

    first commit

--- style.js
+++ style.js
@@ -13,15 +13,15 @@ function switchButton(e){
 
 function switchButton(e){
     if(e.target.checked){
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme','dark');
-    } 
-    else{
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme','light');
+    } 
+    else{
+        document.documentElement.setAttribute('data-theme', 'dark');
+        window.localStorage.setItem('theme','dark');
     }
 }
-
+s
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
 if (currentTheme) {
